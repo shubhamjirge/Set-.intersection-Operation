930e41caea779959aaea6aafd898c0bfeef04738
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-06-08 12:45:38 +0530

    Add files via upload

---
+++ SetintersectionOperation.py
@@ -0,0 +1,12 @@
+"""
+Title     : Set .intersection() Operation
+Subdomain : Sets
+Domain    : Python
+"""
+number_of_english_subscribers = input()
+english_subscribers = set(map(int, input().split()))
+number_of_french_subscribers = input()
+french_subscribers = set(map(int, input().split()))
+print(len(english_subscribers.intersection(french_subscribers)))
+# Alternative solution
+# print(len(english_subscribers & french_subscribers))
